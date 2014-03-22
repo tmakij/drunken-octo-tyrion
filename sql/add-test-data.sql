2c7b7e02a10b9142e0a4a0dea1318ea605ec4676
tmaki <finyou2@gmail.com>
2014-03-22 02:50:47 +0200

    sql

---
+++ sql/add-test-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO kayttaja (nimi, salasana, ryhma) VALUES ('Matti Meikäläinen', '12345', 2), ('Maija Meikäläinen', '54321', 1), ('Esimerkki Erkko', '666', 0);
+INSERT INTO viesti (sisalto, aika, viestiketju) VALUES ('On kyllä hieno!', '1000-1-1 12:51:00', 0), ('No on!', '1000-1-1 12:55:00', 0);
+
---
+++ sql/create-tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE kayttaja (
+    id              SERIAL PRIMARY KEY,
+    nimi            VARCHAR(32) NOT NULL,
+	salasana        TEXT NOT NULL,
+	ryhma           INTEGER NOT NULL
+);
+
+CREATE TABLE viesti (
+    id              SERIAL PRIMARY KEY,
+	sisalto         TEXT NOT NULL,
+	aika            TIME NOT NULL,
+	viestiketju     INTEGER NOT NULL REFERENCES viestiketju(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE viesti_kayttaja (
+	kayttaja            INTEGER PRIMARY KEY NOT NULL REFERENCES kayttaja(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    viesti              INTEGER NOT NULL REFERENCES viesti(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	onlukenut           BOOLEAN NOT NULL
+);
+
+CREATE TABLE viestiketju (
+    id              SERIAL PRIMARY KEY,
+    otsikko         VARCHAR(32) NOT NULL,
+	aihe            INTEGER NOT NULL REFERENCES aihe(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE aihe (
+    id              SERIAL PRIMARY KEY,
+    nimi            VARCHAR(32) NOT NULL
+);
