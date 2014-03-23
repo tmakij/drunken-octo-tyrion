9679fea6012f72f8d36ffcb05caf2208a4289b85
tmaki <finyou2@gmail.com>
2014-03-23 20:40:22 +0200

    sql kayttaja viestissä

--- response.php
+++ response.php
@@ -3,7 +3,8 @@ include 'common/header.php';
 $ketju = $_GET['id'];
 ?>
 <form id="uusiviesti" class="uusiviesti">
-    <p id="otsikko"><?php echo "Otsikko " . $ketju . " (Haetaan tietokannasta)"; ?></p>
+    <p id="otsikko"><?php echo "Otsikko: " . $ketju . " (Haetaan tietokannasta)"; ?></p>
+    Aihe: Rupattelu
     <br>
     <br>
     <textarea rows="25" cols="150" id="sisalto"></textarea>
