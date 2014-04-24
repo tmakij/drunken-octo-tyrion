a8bc4fc08e67a3be88d3944cfaa6a23610eeffd3
tmaki <finyou2@gmail.com>
2014-04-24 16:56:07 +0300

    turhaa pois

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,5 +1,5 @@
-INSERT INTO kayttaja (nimi, salasana, ryhma) VALUES ('Matti Meikäläinen', '12345', 2), ('Maija Meikäläinen', '54321', 1), ('Esimerkki Erkko', '666', 0);
-INSERT INTO aihe (nimi) VALUES ('Rupattelu'), ('Kakut'), ('Yliopisto'), ('Tietojenkäsittelytiede');
+INSERT INTO kayttaja (nimi, salasana, ryhma) VALUES ('NormiKayttaja', '12345', 2), ('admin', 'admin', 1), ('kayttajakiellossa', '666', 0);
+INSERT INTO aihe (nimi) VALUES ('Rupattelu'), ('Spämmi'), ('Yliopisto'), ('Tietojenkäsittelytiede');
 INSERT INTO viestiketju (otsikko, aihe) VALUES ('Uusi foorumi!', 1), ('Tapaaminen', 1);
 INSERT INTO viesti (sisalto, aika, kirjoittaja, viestiketju) VALUES ('On kyllä hieno!', '1000-1-1 12:51:00', 1, 1), ('No on!', '1100-1-1 12:55:00', 2, 1), ('Millon tavataan??', '1050-1-1 13:00:00', 3, 2);
 INSERT INTO viesti_kayttaja (kayttaja, viesti, onlukenut) VALUES (1, 1, FALSE), (2, 2, FALSE);
