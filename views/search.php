89622e6c906fdd884353172e82223f017ce711b4
tmaki <finyou2@gmail.com>
2014-04-25 17:40:05 +0300

    ajanmuoto oikein

--- views/search.php
+++ views/search.php
@@ -4,8 +4,8 @@
     <br>
     Aihe: <?php require 'views/aiheet.php'; ?>
     <br>
-    Aika välillä: <input type="datetime" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" title="Päiväyksen tulee olla VVVV/MM/PP" name="aika_alku" required>-
-    <input pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" title="Päiväyksen tulee olla VVVV/MM/PP" type="datetime" name="aika_loppu" required>
-    <p>Ajan tulee olla muodossa vvvv/kk/pp</p>
+    Aika välillä: <input type="datetime" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" title="Päiväyksen tulee olla VVVV-MM-PP" name="aika_alku" required>-
+    <input pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" title="Päiväyksen tulee olla VVVV-MM-PP" type="datetime" name="aika_loppu" required>
+    <p>Ajan tulee olla muodossa vvvv-kk-pp</p>
     <button type="submit">Hae</button>
 </form> 
